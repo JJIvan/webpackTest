cb5d60070cc4c9d0293eb0dee31bcbe00d2234af
ivan <ivan555ivan555@gmail.com>
2018-03-12 17:23:17 +0800

    'adduglify'

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,15 @@
 // webpack.config.js
 var path = require('path')
+var UglifyJsPlugin = require('uglifyjs-webpack-plugin')
 
 module.exports = {
+  mode: 'development',
   entry: ['./src/index.js'], // 在 index 檔案後的 .js 副檔名是可選的
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js'
-  }
+  }, 
+  plugins: [
+    new UglifyJsPlugin()
+  ]
 }
